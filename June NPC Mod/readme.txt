f1c6566c31a3250b624cf0483e3af3a574b3d0a6
Clare <ccathanlon@gmail.com>
2022-01-29 13:12:42 -0500

    initial commit

---
+++ June NPC Mod/readme.txt
@@ -0,0 +1,87 @@
+June is a traveling pianist who starts working at the Log Cabin Hotel after the
+farmer moves to the valley. He's very polite, but he also seems very private...
+Combined with the fact that he's only there for half the year, will you be able
+to put in the time to see who he really is?
+
+--Gifts--
+(In addition to all Universal gift tastes unless otherwise stated.)
+Loves: Spicy eel, triple shot espresso, coffee bean, apple, bone flute, RSV's
+	Wild Apple Juice
+Likes: Fall and winter crops and forage
+Neutral: Spring + summer crops and forage
+Dislike: Beer
+Hates: Rabbit's foot
+
+--Events--
+Introduction: Exit your farmhouse any day in fall or winter
+0 heart event: Enter the hotel lobby 12-9pm on a day June plays piano
+4 heart event: Enter Ridgeside Village 8:30-12pm on a sunny non-Tuesday
+6 heart event: Enter Cliff 2-8pm on a sunny Mon/Tue/Wen
+7 heart event: Enter Pelican Town between 8 and midnight on a Sunday 
+8 heart event: Enter the beach 12-6pm on a Tuesday in winter
+10 heart event: Same time as 0 heart event, but must be winter
+14 heart event, pt. 1: Unlock the island resort, upgrade your home to
+		       Level 2, then enter the farmhouse between 5 and
+		       10 PM when June is home.
+14 heart event, pt. 2: Go to the beach between 8 PM and midnight on a
+		       non-festival day when the weather is sunny both
+                       in town and on Ginger Island.
+
+--Schedule--
+Spring and Summer: away
+
+Monday:
+6 am - Hotel 2nd floor
+10 am - Heads to Pika's
+1 pm - Leaves Pika's to start walk around Ridgeside Village
+8 pm - Returns to hotel, gets drink from vending machine
+9 pm - Goes to his room on the 2nd floor
+11 pm - Goes to sleep
+
+Tuesday:
+6 am - Hotel 2nd floor
+8 am - Leaves for Pelican Town
+12 pm - Arrives at forest pier in fall, beach in winter
+5 pm - Heads back to hotel
+8 pm - Goes to his room
+11 pm - Goes to sleep
+
+Wednesday:
+6 am - Hotel 2nd floor
+8 am - Heads to Ridge to sit on swing
+12 pm - Heads to Cliff
+2:30 pm - Arrives at edge of Cliff
+8 pm - Heads back to his room
+11 pm - Goes to sleep
+
+Thursday and Friday:
+6 am - Hotel 2nd floor
+10 am - Heads to Pika's
+1 pm - Leaves Pika's to stand outside hotel
+4 pm - Goes to lobby
+5 pm - Begins playing piano
+9 pm - Finishes playing and goes to vending machine
+10 pm - Returns to his room
+11 pm - Goes to sleep
+
+Saturday:
+6 am - Hotel 2nd floor
+8 am - Goes outside hotel
+12 pm - Begins playing piano
+3 pm - Takes break at vending machine
+5 pm - Resumes playing piano
+9 pm - Finishes piano, goes to his room
+10 pm - Goes to sleep
+
+Sunday:
+6 am - Hotel 2nd floor
+8 am - Goes outside hotel
+12 pm - Begins playing piano
+3 pm - Finishes piano, heads to Saloon
+5 pm - Arrives at Saloon
+10 pm - Heads back to hotel
+12 am - Goes to sleep
+
+Once married, he continues working in the hotel from 12 to 6, Thursdays
+through Sundays. On Tuesdays he will go on a walk. On Sundays, he will
+go to the Saloon after work.
---
+++ June Source Code/JuneNPC/ModEntry.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using HarmonyLib;
+
+namespace JuneNPC
+{
+    public class ModEntry : Mod
+    {
+
+        public override void Entry(IModHelper helper)
+        {
+            NPCPatches.Initialize(NPCPatches.Monitor);
+
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+            try
+            {
+                System.Reflection.MethodBase NPC_StartRouteBehavior = AccessTools.Method(typeof(NPC), "startRouteBehavior");
+                System.Reflection.MethodBase NPC_FinishRouteBehavior = AccessTools.Method(typeof(NPC), "finishRouteBehavior");
+
+                harmony.Patch(
+                    original: NPC_StartRouteBehavior,
+                    postfix: new HarmonyMethod(typeof(NPCPatches), nameof(NPCPatches.NPC_StartRouteBehavior_Postfix))
+                    );
+
+                harmony.Patch(
+                    original: NPC_FinishRouteBehavior,
+                    postfix: new HarmonyMethod(typeof(NPCPatches), nameof(NPCPatches.NPC_FinishRouteBehavior_Postfix))
+                    );
+
+                helper.Events.GameLoop.DayEnding += DayEnd;
+            }
+            catch (Exception ex)
+            {
+                Monitor.Log($"Failed in JuneNPC ModEntry stage:\n{ex}", LogLevel.Error);
+            }
+        }
+        private void DayEnd(object sender, DayEndingEventArgs args)
+        {// reset June's sprite at end of day in case day ended while he was playing piano
+            try
+            {
+                NPC June = Game1.getCharacterFromName("June");
+                June.Sprite.SpriteHeight = 32;
+                June.Sprite.SpriteWidth = 16;
+                June.Sprite.ignoreSourceRectUpdates = false;
+                June.Sprite.UpdateSourceRect();
+                June.drawOffset.Value = Vector2.Zero;
+                June.IsInvisible = false;
+            }
+            catch (Exception ex1)
+            {
+                Monitor.Log($"Failed in JuneNPC Day End reset:\n{ex1}", LogLevel.Error);
+            }
+            
+
+        }
+    };
+
+    public class NPCPatches
+    {
+        public static IMonitor Monitor;
+
+        public static void Initialize(IMonitor monitor)
+        {
+            Monitor = monitor;
+        }
+
+
+        public static void NPC_StartRouteBehavior_Postfix(StardewValley.NPC __instance, string behaviorName)
+        {
+            try
+            {
+                switch (behaviorName)
+                {
+                    case "june_piano":
+                        __instance.extendSourceRect(16, 0);
+                        __instance.Sprite.SpriteWidth = 32;
+                        __instance.Sprite.ignoreSourceRectUpdates = false;
+                        __instance.Sprite.currentFrame = 8;
+                        break;
+                }
+            }
+            catch (Exception ex2)
+            {
+                Monitor.Log($"Failed in JuneNPC start postfix:\n{ex2}", LogLevel.Error);
+            }
+        }
+
+        public static void NPC_FinishRouteBehavior_Postfix(StardewValley.NPC __instance, string behaviorName)
+        {
+            try
+            {
+                switch (behaviorName)
+                {
+                    case "june_piano":
+                        __instance.reloadSprite();
+                        __instance.Sprite.SpriteWidth = 16;
+                        __instance.Sprite.SpriteHeight = 32;
+                        __instance.Sprite.UpdateSourceRect();
+                        __instance.drawOffset.Value = Vector2.Zero;
+                        __instance.Halt();
+                        __instance.movementPause = 1;
+                        break;
+                }
+            }
+            catch (Exception ex3)
+            {
+                Monitor.Log($"Failed in JuneNPC finish postfix:\n{ex3}", LogLevel.Error);
+            }
+        }
+    }
+}
